b26b45bea950d4cbe819e7d2adc99497b41d47dc
David Orme <dorme@bradsdeals.com>
2016-11-29 10:01:02 -0600

    Move to Twitter Bootstrap for theming

--- src/clj_boot/docs.clj
+++ src/clj_boot/docs.clj
@@ -4,17 +4,42 @@
 
 
 (defn renderer [{content :entry}]
-  (html5 {:lang "English" :itemscope "" :itemtype "http://schema.org/BlogPosting"}
+  (html5 {:lang "en"}
     [:head
      [:meta {:charset "utf-8"}]
      [:meta {:http-equiv "X-UA-Compatible" :content "IE=edge,chrome=1"}]
      [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0, user-scalable=no"}]
-     [:meta {:name "description" :itemprop "description" :content (str "Library Documentation - " (:name content))}]
-     [:title {:itemprop "name"} (:name content)]
+     [:meta {:name "description" :itemprop "description" :content (str "Library Documentation - " (:name content) " - " (:description content))}]
+     [:title (:name content)]
      [:link {:rel "shortcut icon" :href "/favicon.ico"}]
-     (include-css "/css/app.css")]
+     [:link {:rel "stylesheet" :href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+             :integrity "sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7"
+             :crossorigin "anonymous"}]
+     [:link {:rel "stylesheet" :href "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
+             :integrity "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
+             :crossorigin "anonymous"}]
+
+     ;; font-family: 'Source Sans Pro', sans-serif;
+     ;; font-family: 'Source Code Pro', monospace;
+     ;; font-family: 'Source Serif Pro', serif;
+     [:link {:rel "stylesheet" :href "https://fonts.googleapis.com/css?family=Source+Code+Pro|Source+Sans+Pro|Source+Serif+Pro"}]
+
+     ;; font-family: 'Josefin Slab', serif;
+     ;; font-family: 'Josefin Sans', sans-serif;
+     [:link {:rel "stylesheet" :href "https://fonts.googleapis.com/css?family=Josefin+Sans|Josefin+Slab"}]
+
+     [:script {:src "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+              :integrity "sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+              :crossorigin "anonymous"}]
+     #_(include-css "/css/app.css")]
+
     [:body
-     [:div.row.content
-      [:div.post.small-12.columns
-       [:h1 {:itemprop "name"} (:name content)]
-       (str (:content content))]]]))
+     [:div {:class "jumbotron"}
+      [:div {:class "container"}
+       [:h1 (:name content)]
+       [:p (:description content)]]]
+
+     [:div {:class "container"}
+      [:div {:class "row"}
+       [:div {:class "col-md-12"}
+        (str (:content content))]]]]))
